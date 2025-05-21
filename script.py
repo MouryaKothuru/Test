c6150d6db149a8cb7ccd23e6bbd57e9a576c9a69
MouryaKothuru <mouryakothuru547@gmail.com>
2025-05-21 15:59:31 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,7 @@
+    def main():
+        print("Hello, GitHub Actions")
+        print("This is sample script")
+        print("It runs as part of an automated workflow")
+
+    if __name__ == "__main__":
+      main()
