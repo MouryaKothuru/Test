d63873328f249709bce57391bae45d8b4d478855
MouryaKothuru <mouryakothuru547@gmail.com>
2025-05-21 16:13:12 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
-    def main():
+def main():
         print("Hello, GitHub Actions")
         print("This is sample script")
         print("It runs as part of an automated workflow")
 
-    if __name__ == "__main__":
+if __name__ == "__main__":
       main()
